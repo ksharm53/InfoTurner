1fbd7802abb6b375fad2b5320d06ffec6becd95c
ksharm53 <ksharm53@asu.edu>
2023-09-23 10:34:33 -0700

    Updates

---
+++ precache-manifest.a5a41f2895beb77e350022625249c19d.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7818a834f9d0bad852e1fcafc9132325",
+    "url": "/InfoTurner/index.html"
+  },
+  {
+    "revision": "284330d0ff9d306f314a",
+    "url": "/InfoTurner/static/css/2.8274d451.chunk.css"
+  },
+  {
+    "revision": "7b8a83e231df7a12db9a",
+    "url": "/InfoTurner/static/css/main.abb048df.chunk.css"
+  },
+  {
+    "revision": "284330d0ff9d306f314a",
+    "url": "/InfoTurner/static/js/2.4670f957.chunk.js"
+  },
+  {
+    "revision": "7b8a83e231df7a12db9a",
+    "url": "/InfoTurner/static/js/main.2a8abb94.chunk.js"
+  },
+  {
+    "revision": "3ba740dd11996cfb8c3d",
+    "url": "/InfoTurner/static/js/runtime~main.eca49155.js"
+  },
+  {
+    "revision": "00afc830e9d71e86d88e2e709ab68faa",
+    "url": "/InfoTurner/static/media/1.00afc830.jpg"
+  },
+  {
+    "revision": "841cca7d6981e69440839002d9d2e0da",
+    "url": "/InfoTurner/static/media/1.841cca7d.jpg"
+  },
+  {
+    "revision": "767f0bbb7218666a1fe2fee8e7f83e1f",
+    "url": "/InfoTurner/static/media/3.767f0bbb.jpg"
+  },
+  {
+    "revision": "841cca7d6981e69440839002d9d2e0da",
+    "url": "/InfoTurner/static/media/3.841cca7d.jpg"
+  },
+  {
+    "revision": "767f0bbb7218666a1fe2fee8e7f83e1f",
+    "url": "/InfoTurner/static/media/4.767f0bbb.jpg"
+  },
+  {
+    "revision": "841cca7d6981e69440839002d9d2e0da",
+    "url": "/InfoTurner/static/media/4.841cca7d.jpg"
+  },
+  {
+    "revision": "2770d87181549546bcf430076d152572",
+    "url": "/InfoTurner/static/media/7.2770d871.jpg"
+  },
+  {
+    "revision": "32ce4100989a7a014f8433cb8c2281a2",
+    "url": "/InfoTurner/static/media/bg.32ce4100.jpg"
+  },
+  {
+    "revision": "005c9aa92b564b73b7582cc4f1fa49cb",
+    "url": "/InfoTurner/static/media/fa-brands-400.005c9aa9.woff2"
+  },
+  {
+    "revision": "bb8cd014d7a55672934233c354e1c4a3",
+    "url": "/InfoTurner/static/media/fa-brands-400.bb8cd014.ttf"
+  },
+  {
+    "revision": "675809e48e35c47d51c7d6fcc687ee28",
+    "url": "/InfoTurner/static/media/fa-regular-400.675809e4.ttf"
+  },
+  {
+    "revision": "747442fa76f1d9a31f9a54a2e8a4b448",
+    "url": "/InfoTurner/static/media/fa-regular-400.747442fa.woff2"
+  },
+  {
+    "revision": "0248ab19e74fc3731de14d647db4687a",
+    "url": "/InfoTurner/static/media/fa-solid-900.0248ab19.ttf"
+  },
+  {
+    "revision": "d5e647388e2415268b700d3df2e30a0d",
+    "url": "/InfoTurner/static/media/fa-solid-900.d5e64738.woff2"
+  },
+  {
+    "revision": "58f1253e5639d508a6fe310c97327f82",
+    "url": "/InfoTurner/static/media/fa-v4compatibility.58f1253e.ttf"
+  },
+  {
+    "revision": "f20773a753bf1fbe9ccec04f90a470ea",
+    "url": "/InfoTurner/static/media/fa-v4compatibility.f20773a7.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/InfoTurner/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/InfoTurner/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/InfoTurner/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/InfoTurner/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/InfoTurner/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/InfoTurner/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/InfoTurner/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/InfoTurner/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/InfoTurner/static/media/slick.f97e3bbf.svg"
+  }
+]);
